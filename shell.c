a2476be55fd39926cac33817e6a20eda79fe19e8
Mikko Pelkonen <m.a.pelkonen2@gmail.com>
2017-05-27 12:51:40 +0300

    made code more readable and made the program actually work

--- shell.c
+++ shell.c
@@ -5,34 +5,114 @@
 #include <string.h>
 
 
-int main(int argc, char **argv)
-{
-	shell_loop();
+#define SHELL_RL_BUFSIZE 1024
+#define SHELL_TOK_BUFSIZE 64
+#define SHELL_TOK_DELIM " \t\r\n\a"
 
-	return EXIT_SUCCESS;
+int shell_cd(char **args);
+int shell_help(char **args);
+int shell_exit(char **args);
+
+
+char *builtin_str[] = {
+	"cd",
+	"help",
+	"exit"
+};
+
+
+int (*builtin_func[]) (char **) = {
+	&shell_cd,
+	&shell_help,
+	&shell_exit
+};
+
+
+int shell_num_builtins() {
+	return sizeof(builtin_str) / sizeof(char *);
 }
 
-void shell_loop()
+
+int shell_cd(char **args)
 {
-	char *line;
-	char **args;
+	if (args[1] == NULL) {
+		fprintf(stderr, "shell: expecteded argument to \"cd\"\n");
+	} else {
+		if (chdir(args[1]) != 0) {
+			perror("shell");
+		}
+	}
+	return 1;
+}
+
+
+int shell_help(char **args)
+{
+	int i;
+	printf("A Simple Shell\n");
+	printf("functions built in: \n");
+
+	for (i = 0; i < shell_num_builtins(); i++) {
+		printf("  %s\n", builtin_str[i]);
+	}
+	return 1;
+}
+
+
+int shell_exit(char **args)
+{
+	return 0;
+}	
+
+
+int shell_launch(char **args)
+{
+	pid_t pid;
 	int status;
 
-	do {
-		printf(">");
-		line = shell_read_line();
-		args = shell_split_line(line);
-		status = shell_execute(args);
-
-		free(line);
-		free(args):
-	} while (status);
+	pid = fork();
+	if (pid == 0) {
+		//child process
+		if (execvp(args[0], args) == -1) {
+			perror("shell");
+		}
+		exit(EXIT_FAILURE);
+	} else if (pid < 0) {
+		// error forking
+		perror("shell");
+	} else {
+		// parent process
+		do {
+			waitpid(pid, &status, WUNTRACED);
+		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+	}
+	return 1;
 }
 
-#define SHELL_RL_BUFSIZE 1024
-char *shell_read_line()
+
+int shell_execute(char **args)
 {
-	int bufsize = SHELL_RL_BUFSIZE:
+	int i;
+
+	if (args[0] == NULL) {
+		return 1;
+	}
+
+	for (i = 0; i < shell_num_builtins(); i++) {
+		if (strcmp(args[0], builtin_str[i]) == 0) {
+			return (*builtin_func[i])(args);
+		}
+	}
+	return shell_launch(args);
+}
+
+
+
+
+
+char *shell_read_line(void)
+{
+	int bufsize = SHELL_RL_BUFSIZE;
 	int position = 0;
 	char *buffer = malloc(sizeof(char) * bufsize);
 	int c;
@@ -47,8 +127,10 @@ char *shell_read_line()
 		c = getchar();
 
 		// if end of file, replace with null character and return
-		if (c = EOF || c == "\n") {
-			buffer[position] = "\0";
+		if (c == EOF) {
+			exit(EXIT_SUCCESS);
+		} else if (c == '\n') {
+			buffer[position] = '\0';
 			return buffer;
 		} else {
 			buffer[position] = c;
@@ -67,124 +149,65 @@ char *shell_read_line()
 	}
 }
 
-#define SHELL_TOK_BUFSIZE 64
-#define SELL_TOK_DELIM " \t\r\n\a"
+
+
 char **shell_split_line(char *line)
 {
 	int bufsize = SHELL_TOK_BUFSIZE, position = 0;
 	char **tokens = malloc(bufsize * sizeof(char*));
-	char *token;
+	char *token, **tokens_backup;
 
 	if (!tokens) {
 		fprintf(stderr, "shell:allocation error\n");
 		exit(EXIT_FAILURE);
 	}
 
-	token = strtok(line, SHEL_TOK_DELIM);
+	token = strtok(line, SHELL_TOK_DELIM);
 	while (token != NULL) {
 		tokens[position] = token;
 		position++;
 
 		if (position >= bufsize) {
 			bufsize += SHELL_TOK_BUFSIZE;
+			tokens_backup = tokens;
 			tokens = realloc(tokens, bufsize * sizeof(char*));
 			if (!tokens) {
+				free(tokens_backup);
 				fprintf(stderr, "shell: allocation error\n");
 				exit(EXIT_FAILURE);
 			}
 		}
 
-		token = strtok(NULL, SHEL_TOK_DELIM);
+		token = strtok(NULL, SHELL_TOK_DELIM);
 	}
 	tokens[position] = NULL;
 	return tokens;
 }
 
-int shell_launch(char **args)
+
+void shell_loop(void)
 {
-	pid_t pid, wpid;
+	char *line;
+	char **args;
 	int status;
 
-	pid = fork();
-	if (pid == 0) {
-		//child process
-		if (execvp(args[0], args) == 1) {
-			perror(shell);
-		}
-		exit(EXIT_FAILURE);
-	} else if (pid < 0) {
-		// error forking
-		perror("shell");
-	} else {
-		// parent process
-		do {
-			wpid = waitpid(pid, &status, WUNTRACED);
-		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
-	}
-	return 1;
+	do {
+		printf("Shell> ");
+		line = shell_read_line();
+		args = shell_split_line(line);
+		status = shell_execute(args);
+
+		free(line);
+		free(args);
+	} while (status);
 }
 
-int shell_cd(char **args);
-int shell_help(char **args);
-int shell_exit(char **args);
 
-char *builtin_str[] = {
-	"cd",
-	"help",
-	"exit"
-};
-
-int (*builtin_func[]) (char **) = {
-	&shell_cd,
-	&shell_help,
-	&shell_exit
-};
-
-int shell_num_builtins() {
-	return sizeof(builtin_str) / sizeof(char *);
-}
-
-int shell_cd(char **args)
+int main(int argc, char **argv)
 {
-	if (args[1] == NULL) {
-		fprintf(stderr, "shell: expecteded argument to \"cd\"\n");
-	} else {
-		if (chdir(args[1]) != 0) {
-			perror(shell);
-		}
-	}
-	return 1;
+	shell_loop();
+
+	return EXIT_SUCCESS;
 }
 
-int shell_help(char **args)
-{
-	int i;
-	printf("a simple shell\n");
-	printf("functions built in: \n");
 
-	for (i = 0; i < shell_num_builtins) {
-		printf(" %s\n", builtin_str[i]);
-	}
-	return 1;
-}
-
-int shell_exit(char **args)
-{
-	return 0;
-}	
-
-int shell_execute(char **args)
-{
-	int i;
-
-	if (args[0] == NULL) {
-		return 1;
-	}
-
-	for (i = 0; i < shell_num_builtins(); i++) {
-		if (strcmp(args[0], builtin_str[i]) == 0) {
-			return (*builtin_func[i])(args);
-		}
-	}
-	return shell_launch(args);
-}
